5a2bd13158729f6a281163cc822d0db733bbeee1
Gilbert <ktf2002kzz@gmail.com>
2018-11-07 15:23:40 +0900

    initial commit

--- baekjoon1152/main.c
+++ baekjoon1152/main.c
@@ -8,8 +8,28 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void)
+{
+    int i, count = 0;
+    char arr[1000001];
+    
+    fgets(arr, sizeof(arr), stdin);
+    
+    for(i = 0; i < strlen(arr); i++)
+    {
+        if (arr[i - 1] == ' ' && arr[i] != ' ' && arr[i] != '\n')
+        {
+            count++;
+            //printf("count++ at : %d %d\n",arr[i-1], arr[i]);
+        }
+    }
+    
+    if (arr[0] != ' ')
+        count++;
+    
+    printf("%d \n", count);
     return 0;
 }
+
+
+//    if (arr[strlen(arr) - 2] == ' ') count--;
